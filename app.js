cb4dd2c066e33cea7288f87f9fbddbca6ba54376
Suyog Acharya <asuyog042@gmail.com>
2023-11-21 09:27:43 +0545

    reviews routes restructured

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = 8080;
-const Listing = require("./models/Listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError");
-const { listingSchema, reviewSchema } = require("./Schema.js");
-const Review = require("./models/review.js");
 
 // Set up view engine
 app.set("view engine", "ejs");
@@ -19,8 +15,8 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
 const listings = require("./routes/listing");
+const reviews = require("./routes/review");
 
 // Connect to MongoDB
 MONGO_URL = "mongodb://127.0.0.1:27017/NamasteNooks";
@@ -37,53 +33,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// app.get("/testListing", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "Yak and Yeti",
-//     description: "A beautiful place to stay",
-//     price: 1500,
-//     location: "Babarmahal, Kathmandu",
-//     country: "Nepal",
-//   });
-//   await sampleListing.save();
-//   res.send(sampleListing);
-// });
-
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
 app.use("/listings", listings);
-
-// Review Routes
-app.post(
-  "/listings/:id/reviews",
-  validateReview,
-  wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let review = new Review(req.body.review);
-    listing.reviews.push(review);
-    await review.save();
-    await listing.save();
-    res.redirect(`/listings/${listing._id}`);
-  })
-);
-// deleting review
-
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-  let { id, reviewId } = req.params;
-  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/listings/${id}`);
-}
-));
-
+app.use("/listings/:id/reviews", reviews);
 
 
 // Error Handling
---
+++ routes/review.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync");
+const ExpressError = require("../utils/ExpressError");
+const { reviewSchema } = require("../Schema.js");
+const Listing = require("../models/Listing");
+const Review = require("../models/review.js");
+
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+// Review Routes
+router.post(
+  "/",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let review = new Review(req.body.review);
+    listing.reviews.push(review);
+    await review.save();
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
+// deleting review
+
+router.delete(
+  "/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+module.exports = router;
