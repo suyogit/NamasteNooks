d85814cdd5c922d2bd76e724f44a0c3e1fae67f7
Suyog Acharya <asuyog042@gmail.com>
2023-11-25 17:41:11 +0545

    map marker

--- public/js/map.js
+++ public/js/map.js
@@ -4,6 +4,14 @@ const map = new mapboxgl.Map({
   container: "map", // container ID
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [85.342, 27.6915], // starting position [lng, lat]
+  center: [85.342, 27.6915], // starting position [lng, lat] //coordinates shoulld be here but previous listing donot have coordinates so i have kept it as it is
   zoom: 10.5, // starting zoom
 });
+
+const marker = new mapboxgl.Marker({
+  color: "#FF0000",
+  draggable: true,
+})
+  .setLngLat(coordinates) //listing.geometry.coordinates
+  .addTo(map);
+
